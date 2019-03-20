15e09046e676d273c7664187dbdac5b5a629d862
mitama <loligothick@gmail.com>
2019-03-20 21:23:50 +0900

    add: is_result_with

--- include/Result.hpp
+++ include/Result.hpp
@@ -50,6 +50,25 @@ struct is_result<Result<T, E>> : std::true_type
 template <class T>
 inline constexpr bool is_result_v = is_result<T>::value;
 
+template <class, class...>
+struct is_result_with : std::false_type
+{
+};
+template <class T, class E>
+struct is_result_with<Result<T, E>, Ok<T>> : std::true_type
+{
+};
+template <class T, class E>
+struct is_result_with<Result<T, E>, Err<E>> : std::true_type
+{
+};
+template <class T, class E>
+struct is_result_with<Result<T, E>, Ok<T>, Err<E>> : std::true_type
+{
+};
+template <class T, class... Requires>
+inline constexpr bool is_result_with_v = is_result_with<T, Requires...>::value;
+
 template <class>
 struct is_err_type : std::false_type
 {
