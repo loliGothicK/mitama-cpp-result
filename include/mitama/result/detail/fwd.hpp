e92f2a8405613bcd1545b889240fe4efa4ca227f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-05 09:27:48 +0900

    Support void for mitama::result

--- include/mitama/result/detail/fwd.hpp
+++ include/mitama/result/detail/fwd.hpp
@@ -22,6 +22,14 @@ constexpr mutability operator&&(mutability _1, mutability _2) {
 template < mutability Mut >
 inline constexpr bool is_mut_v = !static_cast<bool>(Mut);
 
+template <class T>
+struct void_to_monostate {
+    using type = std::conditional_t<std::is_void_v<T>, std::monostate, T>;
+};
+
+template <class T>
+using void_to_monostate_t = typename void_to_monostate<T>::type;
+
 template <mutability,
           class = std::monostate,   // success type
           class = std::monostate,   // failure type
@@ -31,11 +39,11 @@ class basic_result;
 
 /// alias template for immutable result
 template <class T = std::monostate, class E = std::monostate>
-using result = basic_result<mutability::immut, T, E>;
+using result = basic_result<mutability::immut, void_to_monostate_t<T>, void_to_monostate_t<E>>;
 
 /// alias template for mutable result
 template <class T = std::monostate, class E = std::monostate>
-using mut_result = basic_result<mutability::mut, T, E>;
+using mut_result = basic_result<mutability::mut, void_to_monostate_t<T>, void_to_monostate_t<E>>;
 
 template <class = std::monostate, class...> class success_t;
 template <class T> success_t(T&&) -> success_t<T>;
