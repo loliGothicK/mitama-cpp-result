3b57ea58e7d18cdd1c4b8f1c9011721780fc3dbf
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-12-31 21:05:59 -0500

    Remove Boost from range_to_maybe.hpp

--- include/mitama/maybe/range_to_maybe.hpp
+++ include/mitama/maybe/range_to_maybe.hpp
@@ -2,8 +2,6 @@
 
 #include <mitama/maybe/maybe.hpp>
 
-#include <boost/hana/functional/overload_linearly.hpp>
-
 #include <iterator>
 #include <type_traits>
 #include <utility>
@@ -24,6 +22,18 @@ struct is_range<
 {
   using type = decltype(*begin(std::declval<T>()));
 };
+
+template <class Range>
+concept is_std_range = requires(Range range) {
+  std::begin(range);
+  std::end(range);
+};
+
+template <class Range>
+concept is_adl_range = requires(Range range) {
+  begin(range);
+  end(range);
+};
 } // namespace mitama::_range_to_maybe_detail
 
 namespace mitama
@@ -32,24 +42,21 @@ template <class Range>
 auto
 range_to_maybe(Range&& range)
 {
-  return std::invoke(
-      boost::hana::overload_linearly(
-          [](auto&& range
-          ) -> decltype(std::begin(range) != std::end(range), maybe(just(*std::begin(std::forward<Range>(range)))))
-          {
-            return std::begin(range) != std::end(range)
-                       ? maybe(just(*std::begin(std::forward<Range>(range))))
-                       : nothing;
-          },
-          [](auto&& range
-          ) -> decltype(begin(range) != end(range), maybe(just(*begin(std::forward<Range>(range)))))
-          {
-            return begin(range) != end(range)
-                       ? maybe(just(*begin(std::forward<Range>(range))))
-                       : nothing;
-          }
-      ),
-      std::forward<Range>(range)
-  );
+  if constexpr (_range_to_maybe_detail::is_std_range<Range>)
+  {
+    return std::begin(range) != std::end(range)
+               ? maybe(just(*std::begin(std::forward<Range>(range))))
+               : nothing;
+  }
+  else if constexpr (_range_to_maybe_detail::is_adl_range<Range>)
+  {
+    return begin(range) != end(range)
+               ? maybe(just(*begin(std::forward<Range>(range))))
+               : nothing;
+  }
+  else
+  {
+    static_assert(!sizeof(Range*), "Unsupported range type"); // always false
+  }
 }
 } // namespace mitama
