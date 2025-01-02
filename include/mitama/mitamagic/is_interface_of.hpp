cea8304f8adfb0399bf5faccf2400ea653eb7159
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-01-01 23:19:37 -0500

    format fix

--- include/mitama/mitamagic/is_interface_of.hpp
+++ include/mitama/mitamagic/is_interface_of.hpp
@@ -4,8 +4,7 @@ namespace mitama::mitamagic
 {
 
 template <typename Interface, typename Derived>
-inline constexpr bool is_interface_of_v = requires(Interface* i, Derived* d) {
-    i = d;
-};
+inline constexpr bool is_interface_of_v =
+    requires(Interface* i, Derived* d) { i = d; };
 
 } // namespace mitama::mitamagic
