d72c9fd1f9365718944436bce671f4949512b64d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-01-12 02:23:35 -0500

    Use format_string for runtime_panic
    This can avoid using fmt::runtime

--- include/mitama/panic.hpp
+++ include/mitama/panic.hpp
@@ -2,11 +2,9 @@
 
 #include <mitama/mitamagic/format.hpp>
 
+#include <fmt/core.h>
 #include <stdexcept>
-#include <string>
-#include <string_view>
 #include <utility>
-#include <variant>
 
 namespace mitama
 {
@@ -16,12 +14,12 @@ class runtime_panic : public std::runtime_error
 public:
   template <class... Args>
   explicit runtime_panic(
-      const char* file, const int line, std::string_view f, Args&&... args
+      const char* file, const int line, fmt::format_string<Args...> f,
+      Args&&... args
   ) noexcept
       : std::runtime_error(fmt::format(
             "runtime panicked at '{}', {}:{}",
-            fmt::format(fmt::runtime(f), std::forward<Args>(args)...), file,
-            line
+            fmt::format(f, std::forward<Args>(args)...), file, line
         ))
   {
   }
